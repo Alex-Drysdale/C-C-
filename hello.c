6158c6b107b9d9f493bb6af12afd575a94d0d113
Alexander Drysdale <drysdalealex0@gmail.com>
2024-07-22 13:46:34 +0100

    current files

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+
+int main() {
+    int arr[0];
+
+    printf("Hello World!");
+    arr[5] = 4, 3, 3, 3, 3; // Yeh I don't know why this is not saying it's wrong with now curly brackets
+    
+    printf("%d", arr ); 
+
+}
+
