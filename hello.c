37da29c75192369f8bfe3c867e8f6b74801f841d
Alexander Drysdale <drysdalealex0@gmail.com>
2024-07-23 12:16:30 +0100

    Read me updates

--- hello.c
+++ hello.c
@@ -2,12 +2,11 @@
 
 
 int main() {
-    int arr[0];
-
-    printf("Hello World!");
-    arr[5] = 4, 3, 3, 3, 3; // Yeh I don't know why this is not saying it's wrong with now curly brackets
     
-    printf("%d", arr ); 
+    int arr[5] = {4, 3, 3, 3, 3}; 
+    printf("Hello World!");
+    
+    printf("%d", arr[0] ); 
 
 }
 
